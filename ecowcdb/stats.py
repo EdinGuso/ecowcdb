514211e7cc4ecf519807b87f367cb4ece42a6646
Edin Guso <edin.guso@epfl.ch>
2023-06-19 14:02:13 +0200

    Add new ecowcdb.stats methods

--- ecowcdb/stats.py
+++ ecowcdb/stats.py
@@ -1,3 +1,7 @@
+"""
+ desc.
+"""
+
 # Standard Library Imports
 from pickle import load
 from typing import Dict, List, Tuple
@@ -24,7 +28,7 @@ class Stats:
         with open(filepath, 'rb') as file:
             self.__results = load(file)
 
-    def pearson_correlation(self, foi: int) -> None:
+    def delay_runtime_correlation(self, foi: int) -> None:
         self.__validation.foi(foi, self.__results)
 
         delays = [item[1] for item in self.__results[foi]]
@@ -32,3 +36,21 @@ class Stats:
 
         correlation, p_value = pearsonr(delays, runtimes)
         print(f'The correlation between delays and runtimes:\n{correlation=}\n{p_value=}')
+
+    def forestsize_delay_correlation(self, foi: int) -> None:
+        self.__validation.foi(foi, self.__results)
+
+        forest_sizes = [len(item[0]) for item in self.__results[foi]]
+        delays = [item[1] for item in self.__results[foi]]
+
+        correlation, p_value = pearsonr(forest_sizes, delays)
+        print(f'The correlation between forest sizes and delays:\n{correlation=}\n{p_value=}')
+
+    def forestsize_runtime_correlation(self, foi: int) -> None:
+        self.__validation.foi(foi, self.__results)
+
+        forest_sizes = [len(item[0]) for item in self.__results[foi]]
+        runtimes = [item[2] for item in self.__results[foi]]
+
+        correlation, p_value = pearsonr(forest_sizes, runtimes)
+        print(f'The correlation between forest sizes and runtimes:\n{correlation=}\n{p_value=}')
